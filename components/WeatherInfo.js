2fa1873b7f3db3f7b44d5e86ac6b20276509c1ed
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2021-04-05 02:41:00 +0200

    [ADD] localisation, details

--- components/WeatherInfo.js
+++ components/WeatherInfo.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native'
+import {colors} from '../utils/index'
+
+const { PRIMARY_COLOR, SECONDARY_COLOR } = colors
 
 export default function WeatherInfo({currentWeather}) {
     const { main : { temp },
             weather: [details],
             name} = currentWeather
-    const { icon } = details
+    const { icon, main, description } = details
     const iconUrl = `https://pbs.twimg.com/profile_images/1173919481082580992/f95OeyEW_400x400.jpg`
     return (
         <View style = {styles.WeatherInfo}>
             <Text>{name}</Text>
             <Image style={styles.weatherIcon} source={{ uri : iconUrl }} />
-            <Text> {temp} </Text>
+            <Text style={styles.textPrimary}> {temp} </Text>
+            <Text style={styles.weatherDescription}>{description} </Text>
+            <Text style={styles.textSecondary}> {main} </Text>
+
         </View>
     )
 }
@@ -20,8 +26,21 @@ const styles = StyleSheet.create({
 WeatherInfo: {
     alignItems: 'center',
 },
+weatherDescription: {
+textTransform: 'capitalize',
+},
 weatherIcon : {
     width: 100,
     height: 100,
 },
+textPrimary : {
+    fontSize : 40,
+    color: PRIMARY_COLOR
+},
+textSecondary: {
+    fontStyle: 20,
+    color: SECONDARY_COLOR,
+    fontWeight: '500',
+    marginTop: 10
+}
 })
