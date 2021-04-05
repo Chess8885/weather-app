f46a2f74298b72b3fb191a816b32e64d992cfe9f
Alexandre Ohayon <alexandre.ohayon@epitech.eu>
2021-04-05 02:49:38 +0200

    [ADD] picker dep, change temp system with picker

--- App.js
+++ App.js
@@ -1,74 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import * as Location from 'expo-location'
-import WeatherInfo from './components/WeatherInfo'
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import WeatherInfo from "./components/WeatherInfo";
+import UnitsPicker from "./components/UnitsPicker";
 
-const WEATHER_API_KEY="b9e12082e2b094e05168d437a9830eba"
-const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+const WEATHER_API_KEY = "b9e12082e2b094e05168d437a9830eba";
+const BASE_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
 
 export default function App() {
-
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [currentWeather, setCurrentWeather] = useState(null)
-  const [unitsSystem, setUnitsSystem] = useState("metric")
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [unitsSystem, setUnitsSystem] = useState("metric");
 
   useEffect(() => {
-    load()
-  }, [])
+    load();
+  }, [unitsSystem]);
   async function load() {
     try {
-      let { status } = await Location.requestPermissionsAsync()
-      if (status !== 'granted') {
-        setErrorMessage("Access to location is needed to run the app")
-        return
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMessage("Access to location is needed to run the app");
+        return;
       }
-      const location = await Location.getCurrentPositionAsync()
-      const { latitude, longitude } = location.coords
-      const weatherURL = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
-      
-      const response = await fetch(weatherURL)
-      const result = await response.json()
+      const location = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = location.coords;
+      const weatherURL = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`;
+
+      const response = await fetch(weatherURL);
+      const result = await response.json();
 
       if (response.ok) {
-        setCurrentWeather(result)
+        setCurrentWeather(result);
       } else {
-        setErrorMessage(result.message)
+        setErrorMessage(result.message);
       }
-      alert(`Latitude : ${latitude}, Longitude : ${longitude}`)
+      alert(`Latitude : ${latitude}, Longitude : ${longitude}`);
     } catch (error) {
-      setErrorMessage(error.message)
+      setErrorMessage(error.message);
     }
   }
 
   if (currentWeather) {
-    const { main : { temp } } = currentWeather
     return (
       <View style={styles.container}>
         <StatusBar style="auto" />
         <View style={styles.main}>
-          <WeatherInfo currentWeather={currentWeather}/>
+          <UnitsPicker unitsSystem={unitsSystem} setUnitsSystem={setUnitsSystem}/>
+          <WeatherInfo currentWeather={currentWeather} />
+        </View>
       </View>
-      </View>
-     )} else {
-      return (
-        <View style={styles.container}>
-        <Text>{ errorMessage }</Text>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
         <StatusBar style="auto" />
       </View>
-      )
-    }
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   main: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
